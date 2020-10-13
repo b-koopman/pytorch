4a96d2d169d0280aa9d144781ac4a86fd182a7c8
Omkar Salpekar <osalpekar@fb.com>
2020-10-13 13:06:04 -0700

    Providing more information while crashing process in async error handling
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/46274
    
    We crash the process in NCCL Async Error Handling if the collective
    has been running for greater than some set timeout. This PR introduces more
    information about the rank and duration the collective ran - we will also
    include details about which collective timed out once #46010 lands.
    ghstack-source-id: 114209305
    
    Differential Revision: [D24200144](https://our.internmc.facebook.com/intern/diff/D24200144/)

--- torch/lib/c10d/ProcessGroupNCCL.cpp
+++ torch/lib/c10d/ProcessGroupNCCL.cpp
@@ -598,10 +598,16 @@ void ProcessGroupNCCL::ncclCommWatchdogInternal() {
         // Check for Timeouts in the WorkNCCL Operations, and abort all
         // communicators accordingly.
         if (work.timedOut()) {
+          auto currentTimepoint = std::chrono::steady_clock::now();
+          auto timeElapsed = std::chrono::duration_cast<std::chrono::milliseconds>(
+              currentTimepoint - work.workStartTime_);
           LOG(INFO)
               << "[Rank " << rank_
-              << "] Watchdog caught collective operation timeout for work: "
-              << work;
+              << "] Watchdog caught collective operation timeout: "
+              << work
+              << " ran for "
+              << timeElapsed.count()
+              << " milliseconds before timing out.";
           std::exception_ptr exception_ptr = std::make_exception_ptr(
               std::runtime_error("NCCL Operation Timed Out"));
           work.setException(exception_ptr);
